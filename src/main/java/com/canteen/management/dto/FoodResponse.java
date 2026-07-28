184be798922788aaa2b1874220c76c91b264f24e
dany <prathapadany@gmail.com>
2026-07-28 15:26:45 +0530

    Added review feature

--- src/main/java/com/canteen/management/dto/FoodResponse.java
+++ src/main/java/com/canteen/management/dto/FoodResponse.java
@@ -10,16 +10,24 @@ import lombok.NoArgsConstructor;
 public class FoodResponse {
 
     private Long id;
+
     private String foodName;
+
     private String category;
+
     private Double price;
-    private String availableDate;
-    private String availableTime;
-    private Integer quantity;
-    private String status;
-    private String canteenId;
-    private String message;
+
     private String imageUrl;
 
-}
+    private String availableDate;
 
+    private String availableTime;
+
+    private Integer quantity;
+
+    private String status;
+
+    private String canteenId;
+
+    private String message;
+}

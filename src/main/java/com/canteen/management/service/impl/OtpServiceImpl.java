cd5bb9051dbd3897bbcf1267e43c5ddade7cef19
dany <prathapadany@gmail.com>
2026-08-01 01:02:21 +0530

    Added review feature

--- src/main/java/com/canteen/management/service/impl/OtpServiceImpl.java
+++ src/main/java/com/canteen/management/service/impl/OtpServiceImpl.java
@@ -34,13 +34,15 @@ public class OtpServiceImpl implements OtpService {
     @Override
     public boolean verifyOtp(String email, String otp) {
 
-        System.out.println("VERIFY EMAIL : " + email);
-        System.out.println("USER OTP : " + otp);
-        System.out.println("SERVER OTP : " + otpMap.get(email));
+        System.out.println("========== OTP VERIFY ==========");
+        System.out.println("EMAIL RECEIVED : " + email);
+        System.out.println("OTP RECEIVED   : " + otp);
+        System.out.println("OTP STORED     : " + otpMap.get(email));
+        System.out.println("================================");
 
-        if (!otpMap.containsKey(email))
+        if (!otpMap.containsKey(email)) {
             return false;
-
+        }
 
         if (System.currentTimeMillis() > expiryMap.get(email)) {
 
@@ -50,15 +52,12 @@ public class OtpServiceImpl implements OtpService {
             return false;
         }
 
-
         boolean valid = otp.equals(otpMap.get(email));
-
 
         if(valid){
             otpMap.remove(email);
             expiryMap.remove(email);
         }
-
 
         return valid;
     }

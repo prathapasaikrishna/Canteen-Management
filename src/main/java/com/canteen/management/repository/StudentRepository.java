e5041d466a4492345366f78f0bbb07feef55ec5a
dany <prathapadany@gmail.com>
2026-08-01 11:12:27 +0530

    Added review feature

--- src/main/java/com/canteen/management/repository/StudentRepository.java
+++ src/main/java/com/canteen/management/repository/StudentRepository.java
@@ -16,4 +16,5 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
 
     Optional<Student> findByStudentId(String studentId);
 
+    Student findByStudent(String studentId);
 }
--- src/main/java/com/canteen/management/service/StudentService.java
+++ src/main/java/com/canteen/management/service/StudentService.java
@@ -1,19 +1,1 @@
-package com.canteen.management.service;
-
-import com.canteen.management.dto.LoginRequest;
-import com.canteen.management.dto.LoginResponse;
-import com.canteen.management.dto.StudentRequest;
-import com.canteen.management.dto.StudentResponse;
-import com.canteen.management.dto.UpdateProfileRequest;
-import com.canteen.management.dto.UpdateProfileResponse;
-
-public interface StudentService {
-
-    StudentResponse saveStudent(StudentRequest studentRequest);
-
-    LoginResponse login(LoginRequest loginRequest);
-
-    UpdateProfileResponse updateProfile(UpdateProfileRequest request);
-
-    StudentResponse getStudentByStudentId(String studentId);
-}
+package com.canteen.management.service;import com.canteen.management.dto.*;public interface StudentService {    StudentResponse saveStudent(StudentRequest studentRequest);    LoginResponse login(LoginRequest loginRequest);    UpdateProfileResponse updateProfile(UpdateProfileRequest request);    StudentResponse getStudentByStudentId(String studentId);    ApiResponse changePassword(ChangePasswordRequest request);}

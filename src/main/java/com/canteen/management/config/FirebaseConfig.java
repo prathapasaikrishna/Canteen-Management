f76d6f66b9acb8102a126beec9c64884b6e16d74
dany <prathapadany@gmail.com>
2026-08-02 15:03:30 +0530

    Use Railway env variable for Firebase

--- src/main/java/com/canteen/management/config/FirebaseConfig.java
+++ src/main/java/com/canteen/management/config/FirebaseConfig.java
@@ -3,39 +3,43 @@ package com.canteen.management.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
-import jakarta.annotation.PostConstruct;
-
-import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
+
     @PostConstruct
     public void init() {
 
-        InputStream serviceAccount =
-                getClass().getClassLoader()
-                        .getResourceAsStream("firebase-service-account.json");
-
-        if (serviceAccount == null) {
-            System.out.println("Firebase json not found");
-            return;
-        }
-
         try {
 
-            FirebaseOptions options =
-                    FirebaseOptions.builder()
-                            .setCredentials(
-                                    GoogleCredentials.fromStream(serviceAccount)
+            String firebaseJson =
+                    System.getenv("FIREBASE_SERVICE_ACCOUNT_JSON");
+
+            if (firebaseJson == null || firebaseJson.isBlank()) {
+                System.out.println("Firebase JSON environment variable not found");
+                return;
+            }
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(
+                            GoogleCredentials.fromStream(
+                                    new ByteArrayInputStream(
+                                            firebaseJson.getBytes(StandardCharsets.UTF_8)
+                                    )
                             )
-                            .build();
+                    )
+                    .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
             }
+
+            System.out.println("Firebase Initialized Successfully");
 
         } catch (Exception e) {
             e.printStackTrace();

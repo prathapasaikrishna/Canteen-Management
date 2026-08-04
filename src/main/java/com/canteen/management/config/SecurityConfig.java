85eeb52a9a9cccd57d1935ac9bbdef6a3447e551
dany <prathapadany@gmail.com>
2026-08-05 00:49:14 +0530

    Added

--- src/main/java/com/canteen/management/config/SecurityConfig.java
+++ src/main/java/com/canteen/management/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                                 "/coupon/**",
                                 "/organization/**",
                                 "/branch/**",
-                                "/food/**"
+                                "/food/branch"
 
 
                         ).permitAll()

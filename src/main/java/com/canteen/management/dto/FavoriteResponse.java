0ce0388e43f4e087191dac5bf4aeffeef7c04856
dany <prathapadany@gmail.com>
2026-08-05 00:15:37 +0530

    Added

--- src/main/java/com/canteen/management/dto/FavoriteResponse.java
+++ src/main/java/com/canteen/management/dto/FavoriteResponse.java
@@ -13,8 +13,30 @@ public class FavoriteResponse {
 
     private Long foodId;
 
-    private Long organizationId;
 
-    private Long branchId;
 
-}
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public Long getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(Long foodId) {
+        this.foodId = foodId;
+    }
+
+    }

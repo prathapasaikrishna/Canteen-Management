796e0004f12b3279208defb734fd4583d49ad7b6
dany <prathapadany@gmail.com>
2026-08-04 15:40:41 +0530

    Added

--- src/main/java/com/canteen/management/dto/WalletPaymentRequest.java
+++ src/main/java/com/canteen/management/dto/WalletPaymentRequest.java
@@ -3,14 +3,13 @@ package com.canteen.management.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class WalletPaymentRequest {
 
     private String studentId;
-    private Long orderId;
+
     private Double amount;
 
 }
